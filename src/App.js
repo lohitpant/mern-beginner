c5e13ed1d7da43c0f004780382e0d50e4942c14d
Lohit Pant46 <lopant46@in.ibm.com>
2021-05-20 20:16:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  const [food,setFood]=useState("")
+  const [days, setDays]=useState(0);
+  const [foodList, setFoodList]=useState([])
+  const [newFoodName, setNewFoodName]=useState("")
+
+  useEffect(()=>{
+    Axios.get("http://localhost:4000/read").then((response)=>{
+      setFoodList(response.data)
+      console.log(response);
+    })
+  },[])
+
+  const addToList=()=>{
+    Axios.post("http://localhost:4000/insert", {      
+      foodName:food,
+      days:days
+    })
+  }
+
+  const updateFood=(id)=>{
+    Axios.put("http://localhost:4000/update", {
+      id:id,
+      newFoodName:newFoodName
+    })
+  }
+
+  const deleteFood=(id)=>{
+    Axios.delete(`http://localhost:4000/delete/${id}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App with MERN</h1>
+      <label>Food Name:</label>
+      <input type="text" onChange={(e)=>setFood(e.target.value)} />
+      <label>Days Since You Ate It:</label>
+      <input type="number" onChange={(e)=>setDays(e.target.value)} />
+      <button onClick={addToList}>Add To DB</button>
+      <hr/>
+      <h1>Food List</h1>
+        {foodList.map((val, key)=>(
+          <div key={key} className="food">
+            {val.foodName} Ate since {val.daysSinceIAte} days
+            <button onClick={()=>deleteFood(val._id)}>Delete</button>
+            <input type="text" placeholder="Type New Food Name Here..." onChange={(e)=>setNewFoodName(e.target.value)} />
+            <button onClick={()=>updateFood(val._id)}>Update</button>
+            </div>
+        ))}
     </div>
   );
 }
